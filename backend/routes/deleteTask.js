300e1df7da09329f1e867684493bbe87eb9f264e
reddymanyam <reddappareddy18@gmail.com>
2025-04-01 23:26:21 +0530

    deleteTask router is updated

--- backend/routes/deleteTask.js
+++ backend/routes/deleteTask.js
@@ -5,7 +5,7 @@ const deleteTaskRouter = express.Router();
 deleteTaskRouter.delete('/deleteTask', async (req, res) => {
     try {
         const userId = req.body._id;
-        const deletedTask = User.findOneAndDelete({ _id: userId });
+        const deletedTask = await User.findOneAndDelete({ _id: userId });
         res.status(201).send("Task deleted succesfully", deletedTask);
     } catch (err) {
         res.status(500).json({ message: "Something Went Wrong", err })
