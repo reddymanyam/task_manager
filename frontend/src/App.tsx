aeaa6cea6c796167e12211413228ed9a47ff6bac
reddymanyam <reddappareddy18@gmail.com>
2025-04-18 22:52:41 +0530

    upd: routing implemented

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Auth from './components/Auth';
 import TaskManager from './components/TaskManager';
 
 const App = () => {
   return (
-    <>
-     <TaskManager />
-    </>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Auth />} />
+        <Route path="/dashboard" element={<TaskManager />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
