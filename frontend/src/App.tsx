70cbf90e902baad5db0bd01d455d98c63e64befc
reddymanyam <reddappareddy18@gmail.com>
2025-04-19 23:58:40 +0530

    protected route is updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Auth from './components/Auth';
 import TaskManager from './components/TaskManager';
+import AuthProvider from './components/AuthProvider';
+import ProtectedRoute from './components/ProtectedRoutes';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Auth />} />
-        <Route path="/dashboard" element={<TaskManager />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Auth />} />
+          <Route path="/dashboard" element={<ProtectedRoute><TaskManager /></ProtectedRoute>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+
   );
 };
 
--- frontend/src/components/AuthProvider.jsx
+++ frontend/src/components/AuthProvider.jsx
@@ -17,4 +17,4 @@ const AuthProvider = ({ children }) => {
     )
 }
 
-export default AuthProvider
+export default AuthProvider;
